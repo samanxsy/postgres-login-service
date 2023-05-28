46e30b8097a79381e2d07306e0999b82749d2946
samanxsy <samanxsy@gmail.com>
2023-05-28 16:12:26 +0200

    Routes and Tests initialized

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,35 @@
+import os
 import unittest
+from flask import Flask
+from app.common import status
+from app.server import home, signup, login, profile
 
 
 class BaseTest(unittest.TestCase):
     """Base test class for all tests in this module."""
-    pass
+    def setUp(self):
+        self.app = Flask("LoginSystem", static_folder="./app/static", template_folder="./app/templates")
+        self.app.config["TESTING"] = True
+        self.client = self.app.test_client()
+        self.app.add_url_rule("/login", "login", login)
+        self.app.add_url_rule("/profile", "profile", profile)
+        self.app.add_url_rule("/signup", "signup", signup)
+        self.app.secret_key = os.environ.get("SESSION_KEY")
+
+
+    def test_signup_rout(self):
+        response = self.client.get("/signup")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn(b"<title>Signup</title>", response.data)
+
+
+    def test_login_rout(self):
+        response = self.client.get("/login")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn(b"<title>Login</title>", response.data)
+
+
+    def test_profile_rout(self):
+        response = self.client.get("/profile")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn(b"<title>Profile</title>", response.data)
