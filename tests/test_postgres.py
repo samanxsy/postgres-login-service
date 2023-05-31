e6609894f3844cb155dbbf05add0776e907b3e74
samanxsy <samanxsy@gmail.com>
2023-05-31 15:36:36 +0200

    CI Check

--- tests/test_postgres.py
+++ tests/test_postgres.py
@@ -8,17 +8,16 @@ from app.postgres import database_connection, hash_password, register_user, user
 class FunctionsTest(unittest.TestCase):
     """Test cases for the postgres.py functions"""
 
-
     def test_database_connection_invalid_credentials(self):
         with self.assertRaises(psycopg2.OperationalError):
             connection = psycopg2.connect(
-                host="127.0.0.1",
+                host="db",
                 port=5432,
                 dbname="postgres",
                 user="invalid_user",
                 password="invalid_password"
             )
-            connection.close()
+            self.assertIsNotNone(connection)
 
 
     def test_hash_password(self):
